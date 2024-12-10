b4b6f8cef2c14c6408ecf960e83ec9fe2a97db45
Arjun gupta <rachnag352@gmail.com>
2024-12-10 20:30:49 +0530

    corrected structuring of contact page

--- Frontend/src/components/pages/Contact.jsx
+++ Frontend/src/components/pages/Contact.jsx
@@ -32,33 +32,31 @@ function Contact() {
       });
   };
   return (
-    <div className="h-svh w-full max-h-svh">
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col h-svh justify-between overflow-x-hidden"
-      >
+    <motion.div
+      initial={{ opacity: 0.0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      className="relative flex flex-col h-svh justify-between overflow-x-hidden"
+    >
+      <div className="flex flex-col min-h-screen">
         {/* Navbar */}
         <Navbar />
-        <section id="contact" className=" max-md:overflow-scroll mt-4">
-          <div className="container mx-auto px-5">
-            <h2 className="text-5xl font-salsa max-md:mb-5  max-md:text-4xl text-center mb-10  bg-clip-text  text-transparent bg-gradient-to-r from-gray-600 to-white">
+
+        {/* Contact Section */}
+        <main id="projects" className="flex-grow flex items-center pt-3">
+          <div className="px-5">
+            <h2 className="text-5xl font-salsa max-md:mb-5 max-md:text-4xl text-center mb-10 bg-clip-text text-transparent bg-gradient-to-r from-gray-600 to-white">
               Contact Me
             </h2>
 
             <div className="flex flex-col text-white md:flex-row justify-between items-start md:items-center gap-10">
-              {/* Contact Form */}
               {/* Contact Information */}
               <div className="relative w-full md:w-1/2">
-                {/* Glass effect background */}
-                <div className="absolute inset-0 bg-white/10 backdrop-blur-lg rounded-lg  "></div>
-
-                {/* Content inside the glass effect */}
+                <div className="absolute inset-0 bg-white/10 backdrop-blur-lg rounded-lg"></div>
                 <div className="relative z-10 shadow shadow-white rounded-lg p-8">
                   <h3 className="text-3xl font-bold mb-4">Get in Touch</h3>
                   <p className="mb-6">
@@ -85,14 +83,15 @@ function Contact() {
                   </ul>
                 </div>
               </div>
-              <div className=" shadow rounded-lg p-8 w-full relative md:w-1/2 shadow-white mb-8">
-                <div className="absolute inset-0 bg-white/10 backdrop-blur-lg rounded-lg  "></div>
 
+              {/* Contact Form */}
+              <div className="relative shadow rounded-lg p-8 w-full md:w-1/2 shadow-white mb-8">
+                <div className="absolute inset-0 bg-white/10 backdrop-blur-lg rounded-lg"></div>
                 <form className="relative z-10" onSubmit={handleSubmit}>
                   <div className="mb-4">
                     <label
-                      className="block text-lg font-semibold mb-2"
                       htmlFor="name"
+                      className="block text-lg font-semibold mb-2"
                     >
                       Name
                     </label>
@@ -103,13 +102,13 @@ function Contact() {
                       value={formData.name}
                       onChange={handleInputChange}
                       required
-                      className="w-full text-black font-bold  px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      className="w-full text-black font-bold px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                   </div>
                   <div className="mb-4">
                     <label
-                      className="block text-lg font-semibold mb-2"
                       htmlFor="email"
+                      className="block text-lg font-semibold mb-2"
                     >
                       Email
                     </label>
@@ -125,8 +124,8 @@ function Contact() {
                   </div>
                   <div className="mb-4">
                     <label
-                      className="block text-lg font-semibold mb-2"
                       htmlFor="message"
+                      className="block text-lg font-semibold mb-2"
                     >
                       Message
                     </label>
@@ -136,8 +135,8 @@ function Contact() {
                       value={formData.message}
                       onChange={handleInputChange}
                       rows="5"
-                      className="w-full text-black font-bold  px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                       required
+                      className="w-full text-black font-bold px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                     ></textarea>
                   </div>
                   <div>
@@ -153,13 +152,13 @@ function Contact() {
               </div>
             </div>
           </div>
-        </section>
+        </main>
+        <ToastContainer />
         <div>
-          <ToastContainer />
+          <Footer />
         </div>
-        <Footer />
-      </motion.div>
-    </div>
+      </div>
+    </motion.div>
   );
 }
 
